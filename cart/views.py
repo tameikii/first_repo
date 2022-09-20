7d22e6a4b406322be6325d09d7bcc53e7cd0a2ca
Takumi <w.tameiki@gmail.com>
2022-09-20 15:55:50 +0900

    lesson8

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
+
+from .models import Cart
 # Create your views here.
+
+
+class CartDetailView(DetailView):
+    model = Cart
+    template_name = "cart/shopping_cart.html"
